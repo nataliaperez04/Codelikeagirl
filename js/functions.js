255bc8b61fd87b6ae1cf2427143b49fef17d0759
nataliaperez04 <Nataliapg710@gmail.com>
2019-12-26 13:08:35 +0100

    colorsss

--- js/functions.js
+++ js/functions.js
@@ -2,14 +2,16 @@ var time= new Date();
 var color;
 var myElement;
 
-if(time == 12) {
+if(time.getMinutes() == 55) {
     color = "rojo";
-} else if (time == 15) {
+} else if (time.getHours() == 15) {
     color = "amarillo";
 } else {
     color = "verde";
 }
 
  myElement = document.getElementById("timeColor");
+ myElement.style.backgroundColor = color;
 
-myElement.innerHTML = color;
+ var userName = prompt("¿Como te llamas?");
+ document.getElementById("userName").innerHTML = userName;
