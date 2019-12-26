86b860fa8e01231a42b6bea95f4d13084d910043
nataliaperez04 <Nataliapg710@gmail.com>
2019-12-26 12:47:39 +0100

    nnnn

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,15 @@
-var time= Date();
+var time= new Date();
 var color;
-if(time==12) {
-    color="rojo";
-} else if(time==15)
-    color="amarillo";
-else if(verde)
+var myElement;
+
+if(time == 12) {
+    color = "rojo";
+} else if (time == 15) {
+    color = "amarillo";
+} else {
+    color = "verde";
+}
+
+ myElement = document.getElementById("timeColor");
+
+myElement.innerHTML = color;
